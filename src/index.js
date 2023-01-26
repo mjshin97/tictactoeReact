ef5ef42ab920f24f328e634cd3d20839d5e3a6b3
Minjun Shin <mjshin97@naver.com>
2023-01-26 10:45:35 +0900

    ninth commit

--- src/index.js
+++ src/index.js
@@ -9,20 +9,58 @@ function Square(props) {
     </button>
   );
 }
-
-class Board extends React.Component {
-  renderSquare(i) {
-    return (
-      <Square
-        value={this.props.squares[i]}
-        onClick={() => this.props.onClick(i)}
-      />
-    );
-  }
-
+/*
+class Square extends React.Component {
   render() {
     return (
+      <button 
+        className="square" 
+        onClick={() => this.props.onClick()} //don't confused by only write'onClick'
+      >
+        {this.props.value}
+      </button>
+    );
+  }
+}
+*/
+
+class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true,
+    };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? "X" : "O";
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+  //modified from here
+  renderSquare(i) {
+    return <Square value={i} />;
+  }
+  //modified untill here
+  render() {
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = "Winner: " + winner;
+    } else {
+      status = "Next player: " + (this.state.xIsNext ? "X" : "O");
+    }
+    //modified from here
+    return (
       <div>
+        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -62,79 +100,17 @@ class Board extends React.Component {
     );
   }
 }
-
+//modified untill here
 class Game extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      history: [
-        {
-          squares: Array(25).fill(null),
-        },
-      ],
-      stepNumber: 0,
-      xIsNext: true,
-    };
-  }
-
-  handleClick(i) {
-    const history = this.state.history.slice(0, this.state.stepNumber + 1);
-    const current = history[history.length - 1];
-    const squares = current.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    squares[i] = this.state.xIsNext ? "X" : "O";
-    this.setState({
-      history: history.concat([
-        {
-          squares: squares,
-        },
-      ]),
-      stepNumber: history.length,
-      xIsNext: !this.state.xIsNext,
-    });
-  }
-
-  jumpTo(step) {
-    this.setState({
-      stepNumber: step,
-      xIsNext: step % 2 === 0,
-    });
-  }
-
   render() {
-    const history = this.state.history;
-    const current = history[this.state.stepNumber];
-    const winner = calculateWinner(current.squares);
-
-    const moves = history.map((step, move) => {
-      const desc = move ? "Go to move #" + move : "Go to game start";
-      return (
-        <li key={move}>
-          <button onClick={() => this.jumpTo(move)}>{desc}</button>
-        </li>
-      );
-    });
-
-    let status;
-    if (winner) {
-      status = "Winner: " + winner;
-    } else {
-      status = "Next player: " + (this.state.xIsNext ? "X" : "O");
-    }
-
     return (
       <div className="game">
         <div className="game-board">
-          <Board
-            squares={current.squares}
-            onClick={(i) => this.handleClick(i)}
-          />
+          <Board />
         </div>
         <div className="game-info">
-          <div>{status}</div>
-          <ol>{moves}</ol>
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
         </div>
       </div>
     );
@@ -147,110 +123,21 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Game />);
 
 function calculateWinner(squares) {
-  const width = [];
-
-  for (let x = 0; x < 25; x++) {
-    width.push(x);
-  }
-
-  var liness = [];
-  var count = 0;
-  var end = -2;
-
-  const lines = new Array();
-
-  for (let k = 0; k < 5; k++) {
-    for (let z = 0; z <= 3; z++) {
-      for (let j = count; j < 25; j++) {
-        liness = width.slice(j, j + 3);
-        lines.push(liness);
-
-        if (end % 3 == 0) {
-          count += 2;
-          break;
-        }
-        break;
-      }
-      count += 1;
-      end += 1;
-    }
-  }
-
-  var height = 0;
-  var heightval = [];
-
-  for (let y = 0; y < 5; y++) {
-    for (let z = 0; z < 5; z++) {
-      heightval.push(height);
-      height += 5;
-      if (height >= 25) {
-        break;
-      }
-    }
-
-    height = (height % 20) - 4;
-  }
-
-  var heightCount = 0;
-  var heightEnd = -2;
-
-  for (let w = 0; w < 5; w++) {
-    for (let e = 0; e <= 3; e++) {
-      for (let r = heightCount; r < 25; r++) {
-        liness = heightval.slice(r, r + 3);
-        lines.push(liness);
-
-        if (heightEnd % 3 == 0) {
-          heightCount += 2;
-          break;
-        }
-        break;
-      }
-      heightCount += 1;
-      heightEnd += 1;
-    }
-  }
-
-  var a = 0;
-  var b = 0;
-  var c = 0;
-  var crossCount = 0;
-  var crossLines = [];
-
-  for (let x = 0; x < 3; x++) {
-    for (let y = 0; y < 3; y++) {
-      a = width.indexOf(y + crossCount);
-      b = width.indexOf(y + 6 * 1 + crossCount);
-      c = width.indexOf(y + 6 * 2 + crossCount);
-      lines.push([a, b, c]);
-    }
-    crossCount += 5;
-  }
-
-  crossCount = 0;
-
-  for (let x = 0; x < 3; x++) {
-    for (let y = 2; y < 5; y++) {
-      a = width.indexOf(y + crossCount);
-      b = width.indexOf(y + 4 * 1 + crossCount);
-      c = width.indexOf(y + 4 * 2 + crossCount);
-      lines.push([a, b, c]);
-    }
-    crossCount += 5;
-  }
-
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
-
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       return squares[a];
     }
   }
-  for (let i = 0; i < 25; i++) {
-    if (squares[i] === null) {
-      return null;
-    }
-  }
-  return 25; //In the case of a tie, return 25
+  return null;
 }
-//reference : https://kamro17.tistory.com/79
